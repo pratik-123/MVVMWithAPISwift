9ebf0198816ee0cbd46d545ae96fb7b35c6d5548
Pratik Lad <bunty@Pratiks-MacBook-Pro.local>
2018-03-06 23:17:55 +0530

    first commit

---
+++ MVVMWithAPISwift/ViewController/TableViewCell/ListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListTableViewCell.swift
+//  MVVMWithAPISwift
+//
+//  Created by Pratik Lad on 06/03/18.
+//  Copyright © 2018 Pratik Lad. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MVVMWithAPISwift/ViewModel/ListViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ListViewModel.swift
+//  MVVMWithAPISwift
+//
+//  Created by Pratik Lad on 06/03/18.
+//  Copyright © 2018 Pratik Lad. All rights reserved.
+//
+
+import Foundation
